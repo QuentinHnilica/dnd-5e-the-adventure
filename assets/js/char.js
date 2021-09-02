95a7af2ae667f7d00f7aa16b35255669eeaba7ef
unknown <Quentinhnilica@gmail.com>
2021-09-01 18:34:09 -0700

    char crattion changes

--- assets/js/char.js
+++ assets/js/char.js
@@ -1,5 +1,6 @@
 var myChar = {} //During char creation use this object to add properties
 var theData
+var raceButtons = []
 
 //general function for getting info from API
 function search(nameKey, myArray){
@@ -8,6 +9,27 @@ function search(nameKey, myArray){
             return myArray[i];
         }
     }
+}
+
+function chooseClass(e){
+    
+}
+
+function displayClass(){
+    for (var i = 0; i < raceButtons.length; i++){
+        raceButtons[i].remove()
+    }
+    fetch(refUrl + apiInfo.classes).then(function(response){
+        response.json().then(function(data){
+            for (var i = 0; i < data.results.length; i++){
+                var newButton = document.createElement('button')
+                newButton.innerHTML = data.results[i].name
+                document.body.appendChild(newButton)
+                raceButtons.push(newButton)
+                newButton.addEventListener('click', chooseClass)
+            }
+        })
+    })
 }
 
 function addRace(e){
@@ -19,6 +41,7 @@ function addRace(e){
                 console.log(data)
                 var raceData = data
                 myChar.race = myRace
+                chooseClass()
             })
         })
     }
@@ -29,12 +52,13 @@ function makeChar(apiInfo, refUrl){
     fetch(refUrl + apiInfo.races).then(function(response){
         response.json().then(function(data){
             theData = data
-            // for (var i = 0; i < data.results.length; i++){
-            //     var newButton = document.createElement('button')
-            //     newButton.innerHTML = data.results[i].name
-            //     document.body.appendChild(newButton)
-            //     newButton.addEventListener('click', addRace)
-            // }
+            for (var i = 0; i < data.results.length; i++){
+                var newButton = document.createElement('button')
+                newButton.innerHTML = data.results[i].name
+                document.body.appendChild(newButton)
+                raceButtons.push(newButton)
+                newButton.addEventListener('click', addRace)
+            }
         })
     })
 }
